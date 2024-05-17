909ca88a0387c85087038f431a76f9d609f85575
knlju <ljubisakn@gmail.com>
2024-05-17 20:41:42 +0200

    remove white text

--- src/components/Results.js
+++ src/components/Results.js
@@ -14,7 +14,7 @@ const Results = ({ answers }) => {
   return (
     <div className="max-w-sm w-full lg:flex mx-auto mt-6">
       <div className="p-10 border border-gray-400 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r flex flex-col justify-between leading-normal">
-        <h2 className="mb-4 text-3xl font-extrabold leading-none tracking-tight text-gray-900 md:text-4xl dark:text-white">
+        <h2 className="mb-4 text-3xl font-extrabold leading-none tracking-tight text-gray-900 md:text-4xl">
           You scored {score} out of {qaArray.length}
         </h2>
         <div className="text-gray-900 font-bold text-xl mb-2">
